98ce2aed77b7a755ff7b661262462dd175cbc0bc
Athharv009 <162335912+Athharv009@users.noreply.github.com>
2025-09-14 11:42:31 +0530

    Display Name done

---
+++ src/DisplayName.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+export default function DisplayName() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFullName(`${name} ${lastName}`);
+  };
+
+  return (
+    <div style={{ margin: "10px" }}>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input
+            type="text"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            required
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <div style={{ marginTop: "20px" }}>
+        {
+            fullName && 
+            <div>
+                Full Name: {fullName}
+            </div>
+        }
+      </div>
+    </div>
+  );
+}
